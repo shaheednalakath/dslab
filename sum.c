ff075ab679f4737ccf34a1b4fade239d5be0978e
muhammed abdul shaheed .n <116936159+shaheednalakath@users.noreply.github.com>
2022-11-09 09:23:55 +0000

    sum of two numbers

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,m,add;
+printf("enter value to n");
+scanf("%d",&n);
+printf("enter value to m");
+scanf("%d",&m);
+add=n+m;
+printf(" sum is %d",add);
+
+return 0;
+}
+
+
