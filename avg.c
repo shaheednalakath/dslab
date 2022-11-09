c7419c13c64bab20f8f1a488316ce5ef489dd702
muhammed abdul shaheed .n <116936159+shaheednalakath@users.noreply.github.com>
2022-11-09 09:19:51 +0000

    average of 3 numbers
    helps us to find average of 3 number

---
+++ avg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter value to a");
+scanf("%d",&a);
+
+printf("enter value to a");
+scanf("%d",&b);
+
+printf("enter value to a");
+scanf("%d",&c);
+
+int avg=(a+b+c)/3;
+printf("avg is %d",avg);
+return 0;
+}
